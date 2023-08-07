bc89178879a0e56f6428f6b03780d479448421f0
imrewce <80151533+imrewce@users.noreply.github.com>
2023-08-07 15:30:20 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
  "strategy": {
  "class": "TopkDropoutStrategy",
  "module_path": "qlib.contrib.telescope.workflow.record_metric" #qlib.contrib.strategy.model_strategy", ###version difference, careful!
- "kwargs": {
+ "kwargs":{
  "model": model,
  "dataset": dataset,
     #model: <MODEL> 
